e4eccdc1da69b79c9ddaffd7903e21b1c84c6c84
Adilet312 <adiletm@hotmail.com>
2020-04-08 23:13:52 -0700

    Add boxes with react.

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+// import React from 'react';
+// /*Black box style*/
+// let BlackBoxStyle = {
+//   width:"200px",
+//   height:"70px",
+//   backgroundColor: "black",
+//   fontSize: "30px",
+//   textAlign: "center",
+//   color: "yellow",
+//   padding: "100px",
+//   float: "left"
+// }
+// /*Red box style*/
+// let RedBoxStyle = {
+//   width:"200px",
+//   height:"70px",
+//   backgroundColor: "red",
+//   fontSize: "30px",
+//   textAlign: "center",
+//   color: "blue",
+//   padding: "100px",
+//   float: "left"
+// }
+// /*Green Box style*/
+// let GreenBoxStyle = {
+//   width:"200px",
+//   height:"70px",
+//   backgroundColor: "green",
+//   fontSize: "30px",
+//   textAlign: "center",
+//   color: "white",
+//   padding: "100px",
+//   float: "left"
+// }
+//
+//
+// let BlackBox = () => <div style = {BlackBoxStyle}> Yellow In Black </div>;
+// let RedBox = () => <div style = {RedBoxStyle}> Blue In Red </div>;
+// let GreenBox = () => <div style = {GreenBoxStyle}> White In Green </div>;
+// function App() {
+//
+//   return (
+//     <div>
+//       <BlackBox/>
+//       <RedBox/>
+//       <GreenBox/>
+//     </div>
+//   );
+// }
+//
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*container for all boxes*/
+let mainBoxStyle = {
+  width:"615px",
+  height:"100px",
+  border: "2px solid #bfbcbc54",
+  backgroundColor:"#d3d3d32e",
+  paddingLeft: "15px",
+  paddingTop: "15px",
+  margin: "100px auto"
+
+}
+/*text style*/
+let textStyle = {
+  position:"absolute",
+  top:"40%",
+  left:"20%"
+}
+/*Black box style*/
+let blackBoxStyle = {
+  width:"200px",
+  height:"80px",
+  backgroundColor: "black",
+  fontSize: "15px",
+  textAlign: "center",
+  color: "yellow",
+  float: "left",
+  position: "relative"
+}
+/*Red box style*/
+let redBoxStyle = {
+  width:"200px",
+  height:"80px",
+  backgroundColor: "red",
+  fontSize: "15px",
+  textAlign: "center",
+  color: "blue",
+  float: "left",
+  marginLeft: "5px",
+  position: "relative"
+}
+/*Green Box style*/
+let greenBoxStyle = {
+  width:"200px",
+  height:"80px",
+  backgroundColor: "green",
+  fontSize: "15px",
+  textAlign: "center",
+  color: "white",
+  float: "left",
+  marginLeft: "5px",
+  position: "relative"
+}
+
+
+let BlackBox = () => <div style = {blackBoxStyle}><span style = {textStyle}>Yellow In Black</span></div>;
+let RedBox = ()   => <div style = {redBoxStyle}><span style = {textStyle}>Blue In Red</span></div>;
+let GreenBox = () => <div style = {greenBoxStyle}><span style = {textStyle}>White In Green</span></div>;
+let App = () => (
+                <div style={mainBoxStyle}>
+                  <BlackBox/>
+                  <RedBox/>
+                  <GreenBox/>
+                </div>
+              );
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+);
